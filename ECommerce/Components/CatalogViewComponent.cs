07956143e1fa53a7f21f6b7bf4259c2f6bc101f7
Anatoliy Novikov <chamskra66er@gmail.com>
2020-09-16 17:07:08 +0300

    added view componet for catalog & styles

---
+++ ECommerce/Components/CatalogViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Models.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Components
+{
+    public class CatalogViewComponent : ViewComponent
+    {
+        public LoginViewModel _model;
+        public CatalogViewComponent()
+        {
+            _model = new LoginViewModel();
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_model);
+        }
+    }
+}
--- ECommerce/wwwroot/js/login.js
+++ ECommerce/wwwroot/js/login.js
@@ -15,3 +15,22 @@ window.addEventListener("click", function (e) {
 		modal.style.display = "none";
 	}
 });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtnClick')) {
+
+        var dropdowns = document.getElementsByClassName("dropClick-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
