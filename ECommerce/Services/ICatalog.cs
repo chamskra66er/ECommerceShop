0945a6cd27530fd54b6226efb229c0afa59c3bef
Anatoliy Novikov <chamskra66er@gmail.com>
2020-09-16 17:15:41 +0300

    added interface icatalog

---
+++ ECommerce/Services/ICatalog.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services
+{
+    public interface ICatalog
+    {
+       IEnumerable<Catalog> GetCatalogs();
+    }
+}
