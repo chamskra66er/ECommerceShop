82161295d3a7e082a3c6ffa315b5d39fd6641b27
Anatoliy Novikov <chamskra66er@gmail.com>
2020-09-17 08:37:09 +0300

    update catalogmodel & migration

--- ECommerce/Data/Catalog.cs
+++ ECommerce/Data/Catalog.cs
@@ -10,5 +10,6 @@ namespace ECommerce.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string AwesomeIcon { get; set; }
+        public IEnumerable<SubCatalog> SubCatalogs { get; set; }
     }
 }
---
+++ ECommerce/Data/Migrations/20200917053337_updateCatalog.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerce.Data.Migrations
+{
+    public partial class updateCatalog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCatalogs_CatalogId",
+                table: "SubCatalogs",
+                column: "CatalogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCatalogs_Catalogs_CatalogId",
+                table: "SubCatalogs",
+                column: "CatalogId",
+                principalTable: "Catalogs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCatalogs_Catalogs_CatalogId",
+                table: "SubCatalogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SubCatalogs_CatalogId",
+                table: "SubCatalogs");
+        }
+    }
+}
