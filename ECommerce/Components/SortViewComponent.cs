8684387747fd123b6b3dd3eba655ec8a150b399b
Anatoliy Novikov <chamskra66er@gmail.com>
2020-09-24 08:58:19 +0300

    added sort viewcomponent & sortmodel

---
+++ ECommerce/Components/SortViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerce.Models.Home;
+
+namespace ECommerce.Components
+{
+    public class SortViewComponent : ViewComponent
+    {
+        private readonly SortModel _sortModel;
+        public SortViewComponent()
+        {
+            _sortModel = new SortModel();
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            _sortModel.CategoryId = id;
+            return View(_sortModel);
+        }
+    }
+}
