8f9e296c0f8236f80d231cd0405b5fcb03279f17
Anatoliy Novikov <chamskra66er@gmail.com>
2020-09-24 11:21:18 +0300

    added FilterModel

---
+++ ECommerce/Models/Home/FilterModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ECommerce.Models.Home
+{
+    public class FilterModel
+    {
+        public int CategoryId { get; set; }
+        public List<string> Brands { get; set; }
+        public double PriceDown { get; set; }
+        public double PriceUp { get; set; }
+    }
+}
