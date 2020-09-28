8affcbea1af807dbd1ef24ee8a019e4f76feec2f
Anatoliy Novikov <chamskra66er@gmail.com>
2020-09-28 15:10:03 +0300

    added favorite interface

---
+++ ECommerce/Services/IFavorite.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services
+{
+    public interface IFavorite
+    {
+        IEnumerable<Favorite> GetFavorites();
+
+        Task AddFavorite(Favorite favorite);
+    }
+}
