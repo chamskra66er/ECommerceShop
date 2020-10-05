11b265e97bf9686632eb64d14cedd8b968871fc1
Anatoliy Novikov <chamskra66er@gmail.com>
2020-10-05 11:07:45 +0300

    added cartViewComponent

---
+++ ECommerce/Components/CartViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerce.Data;
+
+namespace ECommerce.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public CartViewComponent(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_cart);
+        }
+    }
+}
